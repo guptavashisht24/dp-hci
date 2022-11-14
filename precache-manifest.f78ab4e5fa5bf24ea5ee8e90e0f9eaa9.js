12bf9804a928768a230b7715bc3a6d2179bdda31
Vashisht Gupta <vashishtgupta@Vashishts-MBP.lan>
2022-11-13 22:45:53 -0600

    Updates

---
+++ precache-manifest.f78ab4e5fa5bf24ea5ee8e90e0f9eaa9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "06822de9d75ae5b2d0b4",
+    "url": "/hci-dp/static/css/main.b0a00ebb.chunk.css"
+  },
+  {
+    "revision": "06822de9d75ae5b2d0b4",
+    "url": "/hci-dp/static/js/main.06822de9.chunk.js"
+  },
+  {
+    "revision": "067befc5dfa3d400da6c",
+    "url": "/hci-dp/static/js/1.067befc5.chunk.js"
+  },
+  {
+    "revision": "f8c80c2149a0976f8cd5",
+    "url": "/hci-dp/static/css/2.cd450529.chunk.css"
+  },
+  {
+    "revision": "f8c80c2149a0976f8cd5",
+    "url": "/hci-dp/static/js/2.f8c80c21.chunk.js"
+  },
+  {
+    "revision": "deb13d5e913648628e86",
+    "url": "/hci-dp/static/js/runtime~main.deb13d5e.js"
+  },
+  {
+    "revision": "3d497d30688639f37f378ac851f4eeed",
+    "url": "/hci-dp/static/media/target.3d497d30.png"
+  },
+  {
+    "revision": "3e41a4526c861918ad8b515f78f092a7",
+    "url": "/hci-dp/static/media/tomthumb.3e41a452.png"
+  },
+  {
+    "revision": "17635fba6f1b2dc3ce849972e8323199",
+    "url": "/hci-dp/static/media/walmart.17635fba.png"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/hci-dp/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/hci-dp/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "94fc595cc9766c85c77e43398e51b3bc",
+    "url": "/hci-dp/index.html"
+  }
+];
