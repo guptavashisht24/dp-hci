e8b5b0d1548cbfeb1365d45e751541f72f6d392e
Vashisht Gupta <vashishtgupta@Vashishts-MBP.lan>
2022-11-14 00:17:32 -0600

    add tooltip

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -6,6 +6,8 @@ import Video from '../../images/video.png'
 import Header from '../../components/Header';
 import Time from '../../images/time.png'
 import Menu from '../../components/Menu'
+import ReactTooltip from "react-tooltip";
+
 
 function HomePage() {
     return (
@@ -20,39 +22,48 @@ function HomePage() {
                 </div>
             </div>
             <div className="mb">
-                <Link to = "/search">
-                <div className="actions">
-                    <div>
-                        Search An App
+                <Link to="/search">
+                    <div className="actions" data-tip data-for="searchApp">
+                        <div>
+                            Search An App
+                        </div>
+                        <div>
+                            <img src={Search} />
+                        </div>
                     </div>
-                    <div>
-                        <img src={Search} />
-                    </div>
-                </div>
+                    <ReactTooltip id="searchApp" place="top" effect="solid">
+                        Click here to search apps for a particular tasks
+                    </ReactTooltip>
                 </Link>
                 <div>
-                <Link to = "/tutorials">
-                    <div className="actions">
-                        <div>
-                            Search Tutorials
+                    <Link to="/tutorials">
+                        <div className="actions" data-tip data-for="searchVideos">
+                            <div>
+                                Search Tutorials
+                            </div>
+                            <div>
+                                <img src={Video} />
+                            </div>
                         </div>
-                        <div>
-                            <img src={Video} />
-                        </div>
-                    </div>
-                </Link>
+                        <ReactTooltip id="searchVideos" place="top" effect="solid">
+                        Click here to search video tutorials for common apps
+                    </ReactTooltip>
+                    </Link>
                 </div>
                 <div>
-                <Link to = "/user">
-                    <div className="actions">
-                        <div>
-                            Schedule a Session
+                    <Link to="/user">
+                        <div className="actions" data-tip data-for="searchVideos">
+                            <div>
+                                Schedule a Session
+                            </div>
+                            <div>
+                                <img src={Time} />
+                            </div>
                         </div>
-                        <div>
-                            <img src={Time} />
-                        </div>
-                    </div>
-                </Link>
+                        <ReactTooltip id="searchVideos" place="top" effect="solid">
+                         Schedule a session with volunteers for understanding how apps work
+                    </ReactTooltip>
+                    </Link>
                 </div>
             </div>
             <Menu />
