116a8377a3a3258a3f81f4b8f4ada4a6bda8d160
Vashisht Gupta <vashishtgupta@Vashishts-MBP.lan>
2022-11-14 13:28:51 -0600

    add responsive fix

--- src/components/NoMobile/index.js
+++ src/components/NoMobile/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 function NoMobile() {
     return (
         <div className="emptyDiv">
-            This app has been optimized for a mobile viewing experience
+            This app has been optimized for a mobile viewing experience.
             We recommend to use devices &lt;= 480px for best experience
         </div>
     )
