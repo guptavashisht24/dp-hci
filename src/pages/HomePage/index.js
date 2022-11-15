b4f4bfd5198b01dcc878452b386aee38b9f95d5f
Vashisht Gupta <vashishtgupta@Vashishts-MacBook-Pro.local>
2022-11-14 19:08:57 -0600

    add fixes

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,26 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './index.css'
 import { Link } from "react-router-dom";
 import Search from '../../images/search.png'
 import Video from '../../images/video.png'
 import Header from '../../components/Header';
 import Time from '../../images/time.png'
-import Menu from '../../components/Menu'
-import Onboard from "../../components/Onboard";
+import Menu from '../../components/Menu';
+
 
 function HomePage() {
-    const [showOnboard, toggleOnboard] = useState(false)
-
-    useEffect(()=>{
-        if(!localStorage.getItem("show")){
-            setTimeout(()=>{
-                toggleOnboard(true)
-            }, 1000)
-        }
-    })
     return (
         <div>
-            <Header type="noback" />
+            <Header />
+            
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
             <div className="introSection">
                 <div>
                     Welcome
@@ -29,44 +22,43 @@ function HomePage() {
                     What can we help you with today ?
                 </div>
             </div>
-            {showOnboard && <Onboard hide = {toggleOnboard} />}
             <div className="mb">
-                <Link to="/search">
-                    <div className="actions" data-tip data-for="searchApp">
-                        <div>
-                            Search An App
-                        </div>
-                        <div>
-                            <img src={Search} />
-                        </div>
+                <Link to = "/search">
+                <div className="actions">
+                    <div>
+                        Search An App
                     </div>
-                  
+                    <div>
+                    <i class="bi bi-search icon-style"></i>
+                        {/* <img src={Search} /> */}
+                    </div>
+                </div>
                 </Link>
                 <div>
-                    <Link to="/tutorials">
-                        <div className="actions" data-tip data-for="searchVideos">
-                            <div>
-                                Search Tutorials
-                            </div>
-                            <div>
-                                <img src={Video} />
-                            </div>
+                <Link to = "/tutorials">
+                    <div className="actions">
+                        <div>
+                            Search Tutorials
                         </div>
-                        
-                    </Link>
+                        <div>
+                        <i class="bi bi-collection-play icon-style"></i>
+                            {/* <img src={Video} /> */}
+                        </div>
+                    </div>
+                </Link>
                 </div>
                 <div>
-                    <Link to="/user">
-                        <div className="actions" data-tip data-for="searchVideos">
-                            <div>
-                                Schedule a Session
-                            </div>
-                            <div>
-                                <img src={Time} />
-                            </div>
+                <Link to = "/user">
+                    <div className="actions">
+                        <div>
+                            Schedule a Session
                         </div>
-                     
-                    </Link>
+                        <div>
+                        <i class="bi bi-calendar2-check icon-style"></i>
+                            {/* <img src={Time} /> */}
+                        </div>
+                    </div>
+                </Link>
                 </div>
             </div>
             <Menu />
