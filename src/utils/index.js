33b7b0015c95cdf53a254d4b780c4399b0b1e725
Vashisht Gupta <vashishtgupta@Vashishts-MBP.lan>
2022-12-09 11:05:34 -0600

    add back compat check

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,7 +9,13 @@ export const checkConflict = (startDate, startTime, checkDate, checkTime) => {
     const userStart = generateDateFormat(startDate, startTime)
     const userEnd = new Date(userStart.getTime() + (30 * 60000))
     const newStart = generateDateFormat(checkDate, checkTime)
+    const newEnd = new Date(newStart.getTime() + (30 * 60000))
+    //Scheduling ahead conflict
     if( newStart >= userStart && newStart <= userEnd){
+        return true
+    }
+    //Scheduling previous conflict
+    if (userStart >= newStart && userStart <= newEnd){
         return true
     }
     return false
